bb1c737b4a738cc5fbc8c558f08b2cd7a2288225
rubbish9 <142936870+rubbish9@users.noreply.github.com>
2023-08-23 20:06:06 -0300

    Update DOM-JIPP_Controller.h

--- DOM-JIPP_Controller.h
+++ DOM-JIPP_Controller.h
@@ -13,7 +13,7 @@ void inicia();
 void inicia_jogo();
 int peca_inicial();
 void jogar(char jogador);
-void CarregaMesaE(int a);
-void CarregaMesaD(int a);
+void carregaMesaE(int a);
+void carregaMesaD(int a);
 
 
