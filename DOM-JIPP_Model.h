e57bebcdf96060b046cc9a0b2eb5cc15fa23f126
rubbish9 <142936870+rubbish9@users.noreply.github.com>
2023-08-23 21:18:57 -0300

    Update DOM-JIPP_Model.h

--- DOM-JIPP_Model.h
+++ DOM-JIPP_Model.h
@@ -6,5 +6,8 @@ Pedro Evaristo de Oliveira
 Priscilla de Souza Jardim
 */
 
+int mesaE, mesaD;    //extremidades da mesa
+int qtmesa;       //qtde de peças na mesa
+char jvez;      //jogador atual em ação ‘1’ ou ‘2’
 
 
